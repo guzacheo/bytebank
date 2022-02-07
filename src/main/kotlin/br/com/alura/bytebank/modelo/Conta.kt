37109763462d204caf3aa4f7676536f749d9c0a9
guzacheo <gu.zacheo@gmail.com>
2022-02-07 17:21:26 -0300

    Variáveis globais e objects (companio e declarations)

--- src/main/kotlin/br/com/alura/bytebank/modelo/Conta.kt
+++ src/main/kotlin/br/com/alura/bytebank/modelo/Conta.kt
@@ -1,14 +1,30 @@
 package br.com.alura.bytebank.modelo
 
+//criar uma property global com set restrito para contar no numnero de contas criadas
+//var totalContas: Int = 0
+//    private set
+
 abstract class Conta(val titular: Cliente, val numero: Int) {
     var saldo = 0.0
         //protected da acesso ao set do saldo pelas classes filhas
         protected set
 
+    //objeto criado para este uso, permite o uso de uma variavel ""global"" e a deixa privada para alteracao somente pela classe conta
+    //o objeto é tratado como uma property e caso nao seja nomeado, possui o nome "Companio"
+    companion object {
+        var totalContas = 0
+            private set
+    }
+
 //    constructor(titular: String, numero: Int){
 //        this.titular = titular
 //        this.numero = numero
 //    }
+
+    //para contar quantas contas foram criadas sempre quando um objeto tipo conta for inicializado
+    init{
+        totalContas++
+    }
 
     fun deposita(valor: Double) {
         if (valor > 0) {
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,10 @@
+import br.com.alura.bytebank.modelo.Conta
 fun main(args: Array<String>) {
     println()
     testaContasDiferentes()
+
+    println()
+    println("Total de contas criadas: ${Conta.totalContas}")
 }
 
 
