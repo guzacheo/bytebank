fc55d50e614b153dda2ae2896340fe98a287f005
guzacheo <gu.zacheo@gmail.com>
2022-02-07 17:32:06 -0300

    Variáveis globais e objects (companio e declarations), com correção

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,25 @@
+import br.com.alura.bytebank.modelo.Autenticavel
 import br.com.alura.bytebank.modelo.Conta
+import br.com.alura.bytebank.modelo.SistemaInterno
+
 fun main(args: Array<String>) {
     println()
     testaContasDiferentes()
 
     println()
     println("Total de contas criadas: ${Conta.totalContas}")
+    println()
+
+    val fran = object : Autenticavel {
+        val nome: String = "Fran"
+        val cpf: String = "222.222.222-22"
+        val senha: Int = 1000
+
+        override fun auth(senha: Int): Boolean = this.senha == senha
+    }
+
+    val sistemaInterno = SistemaInterno()
+    sistemaInterno.entra(fran, 1000)
 }
 
 
